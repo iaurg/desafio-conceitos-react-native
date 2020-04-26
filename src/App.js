2bde3b1e2bee4204a630ccd3a23c932729456450
Italox <italoaurelior@gmail.com>
2020-04-26 13:47:39 -0300

    :sparkles: Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import {
   SafeAreaView,
@@ -7,18 +8,25 @@ import {
   Text,
   StatusBar,
   StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+  TouchableOpacity
+} from 'react-native'
 
-export default function App() {
-  async function handleLikeRepository(id) {
+export default function App () {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data))
+  }, [])
+
+  async function handleLikeRepository (id) {
     // Implement "Like Repository" functionality
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
       <SafeAreaView style={styles.container}>
+        <FlatList data={repositories} />
         <View style={styles.repositoryContainer}>
           <Text style={styles.repository}>Repository 1</Text>
 
@@ -35,7 +43,7 @@ export default function App() {
             <Text
               style={styles.likeText}
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID='repository-likes-1'
             >
               3 curtidas
             </Text>
@@ -45,63 +53,63 @@ export default function App() {
             style={styles.button}
             onPress={() => handleLikeRepository(1)}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            testID='like-button-1'
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
         </View>
       </SafeAreaView>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: '#7159c1'
   },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: '#fff',
+    padding: 20
   },
   repository: {
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   techsContainer: {
-    flexDirection: "row",
-    marginTop: 10,
+    flexDirection: 'row',
+    marginTop: 10
   },
   tech: {
     fontSize: 12,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    backgroundColor: "#04d361",
+    backgroundColor: '#04d361',
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    color: '#fff'
   },
   likesContainer: {
     marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   likeText: {
     fontSize: 14,
-    fontWeight: "bold",
-    marginRight: 10,
+    fontWeight: 'bold',
+    marginRight: 10
   },
   button: {
-    marginTop: 10,
+    marginTop: 10
   },
   buttonText: {
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
-    padding: 15,
-  },
-});
+    color: '#fff',
+    backgroundColor: '#7159c1',
+    padding: 15
+  }
+})
